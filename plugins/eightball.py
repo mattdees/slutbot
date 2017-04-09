968b4ccf489c6265f278d32873f05bde59ad974c
Matt Dees <matt@lessthan3.net>
2017-04-09 03:42:15 +0000

    Improve for pep8 compliance & pythontidy

--- plugins/eightball.py
+++ plugins/eightball.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import random
 
-class eightball:
+
+class eightball(object):
     def __init__(self, irc):
-        self.registered_events = { '.8ball' : self.eightball }
-        string_fh = open('8ball.strings','r')
-        self.strings =[]
+        self.registered_events = {'.8ball': self.eightball}
+        string_fh = open('8ball.strings', 'r')
+        self.strings = []
         for line in string_fh:
             self.strings.append(line)
         self.irc = irc
+
     def get_events(self):
         return self.registered_events
+
     def eightball(self, channel, arguments, user):
         string = random.choice(self.strings)
         if user == 'tmarkovich':
-        	self.irc.msg(channel, "fuck off, thomas")
-        	return
-        self.irc.msg(channel, string )
+            self.irc.msg(channel, 'fuck off, thomas')
+            return
+        self.irc.msg(channel, string)
